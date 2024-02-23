d86e6195c7b9f5f6cf863b75e56804c66d50329b
Wyatt Johnson <95197026+BeautifulBaguetteBeing@users.noreply.github.com>
2024-02-23 03:30:05 -0500

    ijo mute a!

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function transformInputSame(userInput) {
   userInput = userInput.toLowerCase();
 
@@ -5,62 +9,77 @@ function transformInputSame(userInput) {
 
   if (document.getElementById("chkReplaceAN").checked) {
     transformedInput = transformedInput.replace(/än/g, 'ä̃:');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceEN").checked) {
     transformedInput = transformedInput.replace(/e̞n/g, 'ẽ̞:');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceIN").checked) {
     transformedInput = transformedInput.replace(/in/g, 'ĩ:');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceON").checked) {
     transformedInput = transformedInput.replace(/o̞n/g, 'õ̞:');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceUN").checked) {
     transformedInput = transformedInput.replace(/un/g, 'ũ:');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceAN2").checked) {
     transformedInput = transformedInput.replace(/än/g, 'ä̃n');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceEN2").checked) {
     transformedInput = transformedInput.replace(/e̞n/g, 'ẽ̞n');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceIN2").checked) {
     transformedInput = transformedInput.replace(/in/g, 'ĩn');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceON2").checked) {
     transformedInput = transformedInput.replace(/o̞n/g, 'õ̞n');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceUN2").checked) {
     transformedInput = transformedInput.replace(/un/g, 'ũn');
+    sleep(10)
   }
   
   if (document.getElementById("chkReplaceNP").checked) {
     transformedInput = transformedInput.replace(/np/g, 'mp').replace(/n \'p/g, 'm \'p');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceNJ").checked) {
     transformedInput = transformedInput.replace(/nj/g, 'ɲj').replace(/n \'j/g, 'ɲ \'j');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceNW").checked) {
     transformedInput = transformedInput.replace(/nw/g, 'ŋʷw').replace(/n \'w/g, 'ŋʷ \'w');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceNK").checked) {
     transformedInput = transformedInput.replace(/nk/g, 'ŋk').replace(/n \'k/g, 'ɲ \'k');
+    sleep(10)
   }
 
   if (document.getElementById("chkReplaceNW2").checked) {
     transformedInput = transformedInput.replace(/nw/g, 'nʷw').replace(/n \'w/g, 'nʷ \'w');
+    sleep(10)
   }
   
   transformedInput += "]";
