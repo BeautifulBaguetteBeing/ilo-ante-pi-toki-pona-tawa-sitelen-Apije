c0f190103dec90624706e5789ea06dfc334ee75c
Wyatt Johnson <95197026+BeautifulBaguetteBeing@users.noreply.github.com>
2024-02-23 02:43:21 -0500

    ilo tu li kama wan!

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 function transformInputSame(userInput) {
   userInput = userInput.toLowerCase();
-  let transformedInput = "[" + userInput.replace(/^(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, '\'').replace(/\s(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/g, ' \'').replace(/a/g, 'ä').replace(/e/g, 'e̞').replace(/o/g, 'o̞').replace(/np/g, 'mp').replace(/n p/g, 'm p').replace(/n \'p/g, 'm \'p').replace(/nj/g, 'ɲj').replace(/n j/g, 'ɲ j').replace(/n \'j/g, 'ɲ \'j').replace(/nw/g, 'ŋʷw').replace(/n w/g, 'ŋʷ w').replace(/n \'w/g, 'ŋʷ \'w').replace(/nk/g, 'ŋk').replace(/n k/g, 'ŋ k').replace(/n \'k/g, 'ŋ \'k').replace(/kije̞te̞säntäkälu/g, 'kije̞te̞sänˌtäkälu') + "]";
+
+  let transformedInput = "[" + userInput.replace(/^(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, '\'').replace(/\s(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, ' \'').replace(/kijetesantakalu/g, 'kijetesanˌtakalu').replace(/a/g, 'ä').replace(/e/g, 'e̞').replace(/o/g, 'o̞');
+
+  if (document.getElementById("chkReplaceNP").checked) {
+    transformedInput = transformedInput.replace(/np/g, 'mp').replace(/n \'p/g, 'm \'p');
+  }
+
+  if (document.getElementById("chkReplaceNJ").checked) {
+    transformedInput = transformedInput.replace(/nj/g, 'ɲj').replace(/n \'j/g, 'ɲ \'j');
+  }
+
+  if (document.getElementById("chkReplaceNW").checked) {
+    transformedInput = transformedInput.replace(/nw/g, 'ŋʷw').replace(/n \'w/g, 'ŋʷ \'w');;
+  }
+
+  if (document.getElementById("chkReplaceNK").checked) {
+    transformedInput = transformedInput.replace(/nk/g, 'ŋk').replace(/n \'k/g, 'ɲ \'k');;
+  }
+
+  transformedInput += "]";
   return transformedInput;
 }
 
-function transformInputDifferent(userInput) {
-  userInput = userInput.toLowerCase();
-  let transformedInput = "[" + userInput.replace(/^(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, '\'').replace(/\s(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/g, ' \'').replace(/a/g, 'ä').replace(/e/g, 'e̞').replace(/o/g, 'o̞').replace(/kije̞te̞säntäkälu/g, 'kije̞te̞sänˌtäkälu') + "]";
-  return transformedInput;
-}
 
 function transformText() {
-    var userInput = document.getElementById("user-input").value;
-    var transformedTextSame = transformInputSame(userInput);
-    var transformedTextDifferent = transformInputDifferent(userInput)
-    document.getElementById("transformed-text-same").textContent = transformedTextSame;
-  document.getElementById("transformed-text-different").textContent = transformedTextDifferent;
-}
-
-function toggleDarkMode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+  var userInput = document.getElementById("user-input").value;
+  var transformedTextSame = transformInputSame(userInput)
+  document.getElementById("transformed-text").textContent = transformedTextSame;
 }
