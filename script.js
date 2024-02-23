e463cd54ef5ba7e41c6211c478cf02887585e08c
Wyatt Johnson <95197026+BeautifulBaguetteBeing@users.noreply.github.com>
2024-02-23 03:22:14 -0500

    ijo mute a!

--- script.js
+++ script.js
@@ -3,26 +3,6 @@ function transformInputSame(userInput) {
 
   let transformedInput = "[" + userInput.replace(/^(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, '\'').replace(/\s(?!([aeiou]|[jklmnpstw][aeiou]|[jklmnpstw][aeiou]n)\b)/gm, ' \'').replace(/kijetesantakalu/g, 'kijetesanˌtakalu').replace(/a/g, 'ä').replace(/e/g, 'e̞').replace(/o/g, 'o̞');
 
-  if (document.getElementById("chkReplaceNP").checked) {
-    transformedInput = transformedInput.replace(/np/g, 'mp').replace(/n \'p/g, 'm \'p');
-  }
-
-  if (document.getElementById("chkReplaceNJ").checked) {
-    transformedInput = transformedInput.replace(/nj/g, 'ɲj').replace(/n \'j/g, 'ɲ \'j');
-  }
-
-  if (document.getElementById("chkReplaceNW").checked) {
-    transformedInput = transformedInput.replace(/nw/g, 'ŋʷw').replace(/n \'w/g, 'ŋʷ \'w');
-  }
-
-  if (document.getElementById("chkReplaceNK").checked) {
-    transformedInput = transformedInput.replace(/nk/g, 'ŋk').replace(/n \'k/g, 'ɲ \'k');
-  }
-
-  if (document.getElementById("chkReplaceNW2").checked) {
-    transformedInput = transformedInput.replace(/nw/g, 'nʷw').replace(/n \'w/g, 'nʷ \'w');
-  }
-
   if (document.getElementById("chkReplaceAN").checked) {
     transformedInput = transformedInput.replace(/än/g, 'ä̃:');
   }
@@ -62,6 +42,27 @@ function transformInputSame(userInput) {
   if (document.getElementById("chkReplaceUN2").checked) {
     transformedInput = transformedInput.replace(/un/g, 'ũn');
   }
+  
+  if (document.getElementById("chkReplaceNP").checked) {
+    transformedInput = transformedInput.replace(/np/g, 'mp').replace(/n \'p/g, 'm \'p');
+  }
+
+  if (document.getElementById("chkReplaceNJ").checked) {
+    transformedInput = transformedInput.replace(/nj/g, 'ɲj').replace(/n \'j/g, 'ɲ \'j');
+  }
+
+  if (document.getElementById("chkReplaceNW").checked) {
+    transformedInput = transformedInput.replace(/nw/g, 'ŋʷw').replace(/n \'w/g, 'ŋʷ \'w');
+  }
+
+  if (document.getElementById("chkReplaceNK").checked) {
+    transformedInput = transformedInput.replace(/nk/g, 'ŋk').replace(/n \'k/g, 'ɲ \'k');
+  }
+
+  if (document.getElementById("chkReplaceNW2").checked) {
+    transformedInput = transformedInput.replace(/nw/g, 'nʷw').replace(/n \'w/g, 'nʷ \'w');
+  }
+  
   transformedInput += "]";
   return transformedInput;
 }
